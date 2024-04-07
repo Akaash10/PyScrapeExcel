36ad2e2566ce467e875159885825fce62ee8700a
Akaash B <akaashandrew1@gmail.com>
2024-04-08 02:14:42 +0530

    updated

--- FLIPKART/flipkart.py
+++ FLIPKART/flipkart.py
@@ -4,17 +4,20 @@ import pandas as pd
 import os
 
 #url and response 
-url="https://www.flipkart.com/search?q=apple+mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_10_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_10_na_na_ps&as-pos=1&as-type=RECENT&suggestionId=apple+mobiles%7CMobiles&requestId=27da51f3-59e3-4e91-a4bc-9cd4127ac164&as-backfill=on"
-r=requests.get(url)
-
-#conversion of raw text into html parse document
-soup=BeautifulSoup(r.text,"lxml")
+url=[]
+url.append("https://www.flipkart.com/search?q=apple+mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_10_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_10_na_na_ps&as-pos=1&as-type=RECENT&suggestionId=apple+mobiles%7CMobiles&requestId=27da51f3-59e3-4e91-a4bc-9cd4127ac164&as-backfill=on")
+url.append("https://www.flipkart.com/search?q=samsung+mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_2_4_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_2_4_na_na_na&as-pos=2&as-type=RECENT&suggestionId=samsung+mobiles%7CMobiles&requestId=eedcb30d-9e44-4b87-9d8c-8931870ed8fa&as-backfill=on&page=1")
+url.append("https://www.flipkart.com/search?q=mi+mobile&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_2_10_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_2_10_na_na_ps&as-pos=2&as-type=RECENT&suggestionId=mi+mobile%7CMobiles&requestId=fdf7b47a-0120-4cb8-ae62-75a4b9bf91cc&as-backfill=on")
 
 #find links
 links=[]
-a=soup.find_all("a",class_="ge-49M")
-for i in a:
-    links.append("https://www.flipkart.com"+i.attrs["href"])
+for i in url:
+    #conversion of raw text into html parse document
+    r=requests.get(i)
+    soup=BeautifulSoup(r.text,"lxml")
+    a=soup.find_all("a",class_="ge-49M")
+    for j in a:
+        links.append("https://www.flipkart.com"+j.attrs["href"])
 
 #iterate through links
 names,prices,stars,ratings,reviews,descriptions=[],[],[],[],[],[]
@@ -75,14 +78,15 @@ for i in range(len(names)):
 
 # Check if the file exists
 file_name = "flipkart.xlsx"
+file_path = os.path.join(os.getcwd(), file_name)
 
-if os.path.exists(file_name):
-    existing_sheets = pd.ExcelFile(file_name).sheet_names
+if os.path.exists(file_path):
+    existing_sheets = pd.ExcelFile(file_path).sheet_names
     last_sheet_number = int(existing_sheets[-1].replace("Sheet", ""))
     new_sheet_name = f"Sheet{last_sheet_number + 1}"
     df = pd.DataFrame(products, columns=header)
-    with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
+    with pd.ExcelWriter(file_path, engine='openpyxl', mode='a') as writer:
         df.to_excel(writer, sheet_name=new_sheet_name, index=False)
 else:
     df = pd.DataFrame(products, columns=header)
-    df.to_excel(file_name, index=False)
+    df.to_excel(file_path, index=False)
